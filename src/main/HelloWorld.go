21cb2d15f3c5e4401a9e9adcfeb90cdf77284ded
liguigong <liguigong@joyy.com>
2023-02-10 09:49:22 +0800

    switch语句

--- src/main/HelloWorld.go
+++ src/main/HelloWorld.go
@@ -1,17 +1,33 @@
 package main
 
-import "fmt"
-
-type add func(a, b int) int
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	m := map[string]int{"语文": 0, "数学": 39}
-	m["英文"] = 56
-
-	m3 := map[string]string{
-		"guang": "toqiang",
-		"hss":   "sasa",
+	//fmt.Printf("slice_str", slice_str)
+	var a = 12
+	println(strconv.Itoa(a))
+	switch_condition()
+}
+func add(a int) int {
+	return a + 10
+}
+func switch_condition() {
+	color := "yellow"
+	switch color {
+	case "green":
+		fmt.Println("go")
+	case "red", "yellow": //用逗号分隔多个condition，它们之间是“或”的关系，只需要有一个condition满足就行
+		fmt.Println("stop")
 	}
-	println(m3)
-	fmt.Println(m3)
+
+	//switch后带表达式时，switch-case只能模拟相等的情况；如果switch后不带表达式，case后就可以跟任意的条件表达式
+	switch {
+	case add(5) > 10:
+		fmt.Println("right")
+	default:
+		fmt.Println("wrong")
+	}
 }
