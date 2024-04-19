3038ebeffac0c08bcee83704adf24f21e5782318
liguigong <liguigong@joyy.com>
2024-04-19 09:57:09 +0800

    feat: main函数

--- src/main/HelloWorld.go
+++ src/main/HelloWorld.go
@@ -1,12 +1,22 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	var arr4 = []int{2: 15, 4: 30}
-	by := []byte{49, 228, 189, 160}
-	fmt.Printf("%v", arr4)
-	fmt.Printf("%s", string(by))
+	//var arr4 = []int{2: 15, 4: 30}
+	//by := []byte{49, 228, 189, 160}
+	//fmt.Printf("%v", arr4)
+	//fmt.Printf("%s", string(by))
+	//s := "hello, how are you"
+	//arr := strings.Split(s, " ")
+	//for _, val := range arr {
+	//	println(val)
+	//}
+
+	var d int
+	d = 23
+
+	a := 1
+	b := 2
+	c := a + b
+	println(c, d)
+
 }
