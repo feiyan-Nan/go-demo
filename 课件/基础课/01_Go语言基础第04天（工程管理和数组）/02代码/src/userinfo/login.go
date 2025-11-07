6f464e44c7ca34d2f1614c5ca1d2ffe8adc82ce7
liguigong <liguigong@shopline.com>
2025-11-07 17:32:55 +0800

    chore: 添加课程代码和资源文件- 添加多个课程目录下的 .gitignore 文件，忽略编译产物和示例二进制文件 - 添加 .test 文件用于构建示例代码- 添加课程说明文档，包含资源分享链接信息 - 添加 Go 语言示例代码文件，涵盖基础语法到网络编程等内容 - 添加 GTK 界面示例代码和相关项目文件

--- 课件/基础课/01_Go语言基础第04天（工程管理和数组）/02代码/src/userinfo/login.go
+++
@@ -1,9 +0,0 @@
-package userinfo
-
-import "fmt"
-
-// 如果在不同级别目录下调用函数 函数名首字母需要大写
-func Login() {
-	fmt.Println("用户登陆中。。。")
-	fmt.Println("用户登陆成功")
-}
--- 课件/基础课/01_Go语言基础第04天（工程管理和数组）/02代码/src/userinfo/selectuser.go
+++
@@ -1,7 +0,0 @@
-package userinfo
-
-import "fmt"
-
-func SelectUser() {
-	fmt.Println("查询用户成功")
-}
